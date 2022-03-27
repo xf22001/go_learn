52b3e4e67a8081f6c80bf6cf109641e7297d5c12
xiaofei <alibaba22001@163.com>
2022-03-27 20:20:05 +0800

    update

--- test_c/test_c.go
+++ test_c/test_c.go
@@ -2,11 +2,14 @@ package test_c
 
 import "log"
 
-// #cgo CFLAGS: -g -Wall -I.
-// #cgo LDFLAGS:
 // #include "test_c.h"
 import "C"
 
 func TestC() {
 	log.Println("add(1, 2)", C.add(1, 2))
+}
+
+//export sum
+func sum(a, b C.int) C.int {
+	return a + b
 }
